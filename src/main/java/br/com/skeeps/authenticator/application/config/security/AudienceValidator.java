9db980fc5d13695a56e87389cd892a67ab922ccd
Felipe <mac@MacBook-Pro-de-Mac.local>
2024-02-14 14:32:10 -0300

    configurando audience validator e security config

---
+++ src/main/java/br/com/skeeps/authenticator/application/config/security/AudienceValidator.java
@@ -0,0 +1,25 @@
+package br.com.skeeps.authenticator.application.config.security;
+
+import org.springframework.security.oauth2.core.*;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.util.Assert;
+
+import java.util.List;
+
+public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+    private final String audience;
+
+    AudienceValidator(String audience) {
+        Assert.hasText(audience, "audience is null or empty");
+        this.audience = audience;
+    }
+
+    public OAuth2TokenValidatorResult validate(Jwt jwt) {
+        List<String> audiences = jwt.getAudience();
+        if (audiences.contains(this.audience)) {
+            return OAuth2TokenValidatorResult.success();
+        }
+        OAuth2Error err = new OAuth2Error(OAuth2ErrorCodes.INVALID_TOKEN);
+        return OAuth2TokenValidatorResult.failure(err);
+    }
+}
